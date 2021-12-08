5c8a2fe0b972d8592fdc450cbacaa1b568232711
Dastaan007 <93420321+Suhail-007@users.noreply.github.com>
2021-12-08 19:50:24 +0530

    Add files via upload
    Added more categories

--- bmi.js
+++ bmi.js
@@ -1,11 +1,38 @@
 function isBmi() {
+	
 		let weight = parseFloat(document.querySelector('#weight').value);
 		let height = parseFloat(document.getElementById('height').value);
-			
-		let imu = weight / (height/100)**2;
-		 		 
-		let p = document.getElementById('txt');
-		p.innerHTML = imu.toFixed(2)
+		
+		let bmi = weight / (height/100)**2;	 
+		bmi = bmi.toFixed(2);
+		
+		let output = document.getElementById('output');
+		
+		if (bmi > 0 && bmi <= 15) {
+				output.innerText = `Your BMI is: ${bmi} Your weight is Severely	Underweight`;
+		} else if (bmi > 15 && bmi <= 18.9) {
+				output.innerText = `Your BMI is: ${bmi} Your weight is	Underweight`;
+		} else if (bmi > 18.9 && bmi <= 24.9) {
+				output.innerText = `Your BMI is: ${bmi} Your weight is Healthy`;
+		} else if (bmi > 25 && bmi <= 29.9) {
+				output.innerText =  `Your BMI is: ${bmi} Your weight is Overweight`;
+		} else if (bmi > 30 && bmi <= 35) {
+				output.innerText =  `Your BMI is: ${bmi} Your weight is Overweight`;
+		} else if (bmi > 35 && bmi <= 40) {
+				output.innerText =  `Your BMI is: ${bmi} Your weight is Overweight "Obese Class I"`;
+		} else if (bmi > 40 && bmi <= 45) {
+				output.innerText =  `Your BMI is: ${bmi} Your weight is Overweight "Obese Class II"`;
+		} else if (bmi > 45 && bmi <= 50) {
+				output.innerText =  `Your BMI is: ${bmi} Your weight is Overweight "Obese Class III"`;
+		} else if (bmi > 50 && bmi <= 55) {
+				output.innerText =  `Your BMI is: ${bmi} Your weight is Overweight "Obese Class IV"`;
+		} else if (bmi > 55 && bmi <= 60) {
+				output.innerText =  `Your BMI is: ${bmi} Your weight is Overweight "Obese Class V"`;
+		} else if (bmi > 60) {
+				output.innerText =  `Your BMI is: ${bmi} Your weight is Overweight "Obese Class VI"`;
+		} else {
+				output.innerHTML = 'Please type valid numbers.'
+		}
 		
 }
 
